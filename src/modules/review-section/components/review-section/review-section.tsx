bea4913305ee000fbcae8e69c3050e21e2d72ae1
darker <darkerros@yandex.ru>
2023-09-29 12:31:08 +0300

    refactor: refactor rewiev section with new section component

--- src/modules/review-section/components/review-section/review-section.tsx
+++ src/modules/review-section/components/review-section/review-section.tsx
@@ -2,18 +2,16 @@ import styles from './review-section.module.scss';
 
 import { reviews } from "@consts/reviews";
 
+import { Section } from "@components/section";
 import { CommentCard } from "@components/comment-card";
 
 const ReviewSection = () => {
   return (
-    <section className={styles.reviewSection}>
-      <div className={styles.reviewSection__content}>
-        <h2 className={styles.reviewSection__title}>Отзывы наших клиентов</h2>
-        <div className={styles.reviewSection__comentsList}>
-          {reviews.map(comment => (<CommentCard key={comment.name} comment={comment}/>))}
-        </div>
+    <Section title={'Отзывы наших клиентов'}>
+      <div className={styles.reviewSection__comentsList}>
+        {reviews.map(comment => (<CommentCard key={comment.name} comment={comment}/>))}
       </div>
-    </section>
+    </Section>
   );
 };
 
